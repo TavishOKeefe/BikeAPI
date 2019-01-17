a1598036a3c7c68fbc2a6cde1f7eb75f2102e7bb
Charley McGowan and Tavish OKeefe <charley@>
2019-01-16 16:48:51 -0800

    add functionality

--- src/bike.js
+++ src/bike.js
@@ -2,9 +2,9 @@
 
 class Bike {
   constructor() {
+    this.bikeIndexId;
     this.title;
     this.serial;
-    this.BikeIndexId;
     this.manufacturer;
     this.colors;
     this.year;
@@ -13,10 +13,10 @@ class Bike {
     this.stolenDate;
   }
 
-  searchAPIforBikeInfo(serial, manufacturer, location, distance = 10,stolenness = "stolen") {
+  searchAPIforBikeInfo(serial, manufacturer, location, distance = 10,stolenness = 'stolen') {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&serial=${serial}&manufacturer=${manufacturer}&location=${location}&distance=${distance}&stolenness=${stolenness}&access_token=${process.env.bikeAPIkey}`;
+      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&serial=${serial}&manufacturer=${manufacturer}&location=${location}&distance=${distance}&stolenness=${stolenness}&access_token=${process.env.BIKE_API}`;
       request.onload = function () {
         if (this.status === 200){
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
+const loadGoogleMapsApi = require('load-google-maps-api');
 import { Bike } from './bike.js';
+import { ApiMap } from './apiMap.js'
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+    $('#mapButton').click(function(){
+      loadGoogleMapsApi({ key: process.env.GOOGLE_MAPS_API }).then(function (googleMaps) {
+        new googleMaps.Map(document.querySelector('#map'), {
+          center: {
+            lat: 40.7484405,
+            lng: -73.9944191
+          },
+          zoom: 12
+        });
+      }).catch(function (error) {
+        console.error(error)
+      });
+    });
 
   $('#submitBikeInfo').click(function(event) {
     console.log('reached.');
@@ -18,16 +33,24 @@ $(document).ready(function() {
     let newBike = new Bike();
     let promise = newBike.searchAPIforBikeInfo(serial,manufacturer,location,distance,stolenness);
 
-
-      promise.then(function (response) {
-        console.log(response)
-        let body = JSON.parse(response);
-        $('table#apiTableOutput').text('');
-        $('table#apiTableOutput').append("<tr><th>Title</th><th>Serial</th></tr>");
-        body.bikes.forEach(function(bike){
-          $('#apiTableOutput').append('<tr><td>'+ bike.title + '</td> <td>' + bike.serial + '</td></tr>')
-          $('#apiTableOutput').append('</table>')
-        });
+    let bikeList = [];
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      $('table#apiTableOutput').text('');
+      $('table#apiTableOutput').append("<tr><th>Title</th><th>Serial</th><th>Stolen Location</th><</tr>");
+      body.bikes.forEach(function(bike){
+        $('#apiTableOutput').append('<tr><td class="bikeTitle">'+ bike.title + '</td> <td class="bikeSerial">' + bike.serial + '</td> <td class="bikeSolenLocation">' + bike.stolen_location + '</td></tr>');
+        $('#apiTableOutput').append('</table>');
+        let newBikeForList = new Bike();
+        newBikeForList.bikeIndexId = bike.id;
+        newBikeForList.title = bike.title;
+        newBikeForList.serial = bike.serial;
+        newBikeForList.manufacturer = bike.manufacturer_name;
+        newBikeForList.stolenDate = new Date(parseInt(bike.date_stolen)*1000);
+        newBikeForList.stolenLocation = bike.stolen_location;
+        bikeList.push(newBikeForList);
       });
     });
+    // forEach(function(bikeList))
+  });
 });
